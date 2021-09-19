c04a108e907347c9b918beca98ab8c3f5469a64b
Łukasz Piotr Staniszewski <l.staniszewski@stud.elka.pw.edu.pl>
2021-09-19 17:08:01 +0200

    first things first

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import discord
+import re
+import os
+
+BOTCHANNEL_ID = os.getenv("BOTCHANNEL_ID")
+WIDZISZMNIE_EMOTE = os.getenv("WIDZISZMNIE_EMOTE")
+APP_ID = os.getenv("APP_ID")
+client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    if message.channel.id == BOTCHANNEL_ID:
+        print(re.sub(r'#[0-9]+','',str(message.author)) + " wrote: " + message.content)
+    if message.content.find("!brek") != -1:
+        await message.channel.send("Widzisz mnie?! 👀")
+    if message.content.find("!gif") != -1:
+        await message.channel.send(r"https://tenor.com/view/starege-dobrze-dobrzebrek-ca%C5%82y%C5%82eb-%C5%82eb-gif-22915280")
+    if message.content.find(WIDZISZMNIE_EMOTE) != -1 and str(message.author) != "DJ WidziszMnie#0843":
+        await message.add_reaction(WIDZISZMNIE_EMOTE)
+
+
+client.run(APP_ID)
